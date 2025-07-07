9c0e9ef1a6de02959f74beb083977bc823ff7a39
KaanOzen-mF <kaanozn98@gmail.com>
2025-07-07 11:51:25 +0300

    Add HomePage component with initial content

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className="text-[var(--font-size-heading-lg)]">Home Page</h1>
+      <p className="text-[var(--font-size-body-md)] mt-[var(--spacing-200)]">
+        Welcome to the Entertainment Web App!
+      </p>
+    </div>
+  );
+}
