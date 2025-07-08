dc06e3ee55bc4584254909d9e63c87c8a3e3aa4e
KaanOzen-mF <kaanozn98@gmail.com>
2025-07-08 17:05:58 +0300

    Refactor layout.tsx to use Outfit font, improving styling consistency and layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const outfit = Outfit({ subsets: ["latin"], weight: ["300", "500"] });
 
 export const metadata: Metadata = {
   title: "Entertainment Web App",
-  description: "Frontend for the Entertainment Web App",
+  description: "A full-stack entertainment web application.",
 };
 
 export default function RootLayout({
@@ -20,11 +17,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.className} bg-[#10141E] text-white`}>
-        <div className="flex flex-col md:flex-row min-h-screen">
-          <Navbar />
-
-          <main className="flex-1 p-6 md:p-8">{children}</main>
+      <body
+        className={`${outfit.className} flex min-h-screen items-center justify-center bg-darkBlue text-white`}
+      >
+        <div className="h-[90vh] w-full">
+          <div className="flex h-full flex-col md:flex-row">
+            <Navbar />
+            <main className="flex-grow overflow-y-auto p-4 md:p-6 lg:p-0">
+              {children}
+            </main>
+          </div>
         </div>
       </body>
     </html>
