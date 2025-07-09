ed1af493cb3513b1d60989479ab1ced88d01d7b2
KaanOzen-mF <kaanozn98@gmail.com>
2025-07-09 17:21:15 +0300

    Refactor Home component to improve structure and integrate Trending data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-export default function HomePage() {
+// src/app/page.tsx
+
+import Trending from "@/components/Trending";
+import data from "../../data.json";
+
+export default function Home() {
+  const trendingData = data.filter((item) => item.isTrending);
+
   return (
-    <div className="text-white">
-      <h1 className="text-[var(--font-size-heading-lg)]">Home Page</h1>
-      <p className="text-[var(--font-size-body-md)] mt-[var(--spacing-200)]">
-        Welcome to the Entertainment Web App!
-      </p>
-    </div>
+    <section>
+      {/* Search Bar */}
+
+      <Trending data={trendingData} />
+
+      {/* Suggestion */}
+    </section>
   );
 }
