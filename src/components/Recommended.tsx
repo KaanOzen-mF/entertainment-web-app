617b35afc3676f46a9967130dc5a7cf05e554fcd
KaanOzen-mF <kaanozn98@gmail.com>
2025-07-10 17:16:27 +0300

    Add Recommended component to display personalized suggestions

---
+++ src/components/Recommended.tsx
@@ -0,0 +1,26 @@
+// src/components/Recommended.tsx
+
+import { MediaContent } from "../../types";
+import ShowCard from "./ShowCard";
+
+type Props = {
+  data: MediaContent[];
+};
+
+const Recommended = ({ data }: Props) => {
+  return (
+    <section className="mt-6">
+      <h1 className="text-xl font-light text-white mb-6">
+        Recommended for you
+      </h1>
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-8 lg:mr-4">
+        {/* 3. Veri üzerinde map ile dönüp her öğe için bir ShowCard oluşturuyoruz. */}
+        {data.map((item) => (
+          <ShowCard key={item.title} item={item} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Recommended;
