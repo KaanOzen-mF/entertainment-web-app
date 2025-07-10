a3bf55bf8e5f61167a164f31cea1c3f8db916b38
KaanOzen-mF <kaanozn98@gmail.com>
2025-07-10 17:15:55 +0300

    Add MediaContent type definition to define media properties and structure

---
+++ types.ts
@@ -0,0 +1,21 @@
+// src/types.ts
+
+export type MediaContent = {
+  title: string;
+  thumbnail: {
+    trending?: {
+      small: string;
+      large: string;
+    };
+    regular: {
+      small: string;
+      medium: string;
+      large: string;
+    };
+  };
+  year: number;
+  category: string;
+  rating: string;
+  isBookmarked: boolean;
+  isTrending: boolean;
+};
