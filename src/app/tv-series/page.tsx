df03629255c67e97c721bf82ccc8b46077d8547e
KaanOzen-mF <kaanozn98@gmail.com>
2025-07-11 13:28:12 +0300

    Refactor TvSeriesPage component to improve structure and enhance content display

--- src/app/tv-series/page.tsx
+++ src/app/tv-series/page.tsx
@@ -1,10 +1,25 @@
-export default function TVSeriesPage() {
+// src/app/tv-series/page.tsx
+
+import data from "../../../data.json";
+import { MediaContent } from "../../../types";
+import ShowCard from "@/components/ShowCard";
+
+const TvSeriesPage = () => {
+  const tvSeries = data.filter(
+    (item) => item.category === "TV Series"
+  ) as MediaContent[];
+
   return (
-    <div>
-      <h1 className="text-[var(--font-size-heading-lg)]">TV Series Page</h1>
-      <p className="text-[var(--font-size-body-md)] mt-[var(--spacing-200)]">
-        This page will display all TV series.
-      </p>
-    </div>
+    <section className="mt-8">
+      <h1 className="text-xl font-light text-white mb-6">TV Series</h1>
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-8">
+        {tvSeries.map((series) => (
+          <ShowCard key={series.title} item={series} />
+        ))}
+      </div>
+    </section>
   );
-}
+};
+
+export default TvSeriesPage;
