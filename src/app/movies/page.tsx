58b8bd73517ef0cfd75843d2b640717ac65452e5
KaanOzen-mF <kaanozn98@gmail.com>
2025-07-11 13:28:07 +0300

    Refactor MoviesPage component to improve structure and enhance readability

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,10 +1,25 @@
-export default function MoviesPage() {
+// src/app/movies/page.tsx
+
+import data from "../../../data.json";
+import { MediaContent } from "../../../types";
+import ShowCard from "@/components/ShowCard";
+
+const MoviesPage = () => {
+  const movies = data.filter(
+    (item) => item.category === "Movie"
+  ) as MediaContent[];
+
   return (
-    <div>
-      <h1 className="text-[var(--font-size-heading-lg)]">Movies Page</h1>
-      <p className="text-[var(--font-size-body-md)] mt-[var(--spacing-200)]">
-        This page will display all movies.
-      </p>
-    </div>
+    <section>
+      <h1 className="text-xl font-light text-white mb-6 mt-8">Movies</h1>
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-8">
+        {movies.map((movie) => (
+          <ShowCard key={movie.title} item={movie} />
+        ))}
+      </div>
+    </section>
   );
-}
+};
+
+export default MoviesPage;
