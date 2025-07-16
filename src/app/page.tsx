76d2d20a1c60c7012817cd5006b3920ad7cddb4e
KaanOzen-mF <kaanozn98@gmail.com>
2025-07-16 15:52:37 +0300

    Refactor Home component to implement search functionality and improve data filtering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,61 @@
 // src/app/page.tsx
+"use client";
 
-import Trending from "@/components/Trending";
+import { useState } from "react";
 import data from "../../data.json";
+import Trending from "@/components/Trending";
 import Recommended from "@/components/Recommended";
+import SearchInput from "@/components/SearchInput";
+import { MediaContent } from "../../types";
+import ShowCard from "@/components/ShowCard";
 
 export default function Home() {
-  const trendingData = data.filter((item) => item.isTrending);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const isSearching = searchTerm.length > 0;
+
+  const filteredData = isSearching
+    ? data.filter((item) =>
+        item.title.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : data;
+
+  const trendingData = filteredData.filter(
+    (item) => item.isTrending
+  ) as MediaContent[];
+  const recommendedData = filteredData.filter(
+    (item) => !item.isTrending
+  ) as MediaContent[];
 
   return (
-    <section>
-      {/* Search Bar */}
+    <main>
+      <SearchInput
+        placeholder="Search for movies or TV series"
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
 
-      <Trending data={trendingData} />
+      {!isSearching && <Trending data={trendingData} />}
 
-      {/* Suggestion */}
-      <Recommended data={data} />
-    </section>
+      {isSearching ? (
+        <section>
+          <h1 className="text-xl font-light text-white mb-6">
+            Found {filteredData.length} results for &apos;{searchTerm}&apos;
+          </h1>
+
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-8">
+            {filteredData.map((item) => (
+              <ShowCard key={item.title} item={item as MediaContent} />
+            ))}
+          </div>
+        </section>
+      ) : (
+        <Recommended data={recommendedData} />
+      )}
+    </main>
   );
 }
