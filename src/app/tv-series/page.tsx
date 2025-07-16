1c287123b07663d017119eedbc89495f9adbd8a8
KaanOzen-mF <kaanozn98@gmail.com>
2025-07-16 15:52:51 +0300

    Implement search functionality in TvSeriesPage component for improved TV series filtering

--- src/app/tv-series/page.tsx
+++ src/app/tv-series/page.tsx
@@ -1,20 +1,44 @@
 // src/app/tv-series/page.tsx
-
+"use client";
 import data from "../../../data.json";
 import { MediaContent } from "../../../types";
 import ShowCard from "@/components/ShowCard";
+import { useState } from "react";
+import SearchInput from "@/components/SearchInput";
 
 const TvSeriesPage = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
   const tvSeries = data.filter(
     (item) => item.category === "TV Series"
   ) as MediaContent[];
 
+  const filteredTvSeries = searchTerm
+    ? tvSeries.filter((series) =>
+        series.title.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : tvSeries;
+
   return (
-    <section className="mt-8">
-      <h1 className="text-xl font-light text-white mb-6">TV Series</h1>
+    <section>
+      <SearchInput
+        placeholder="Search for TV series"
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+
+      <h1 className="text-xl font-light text-white mb-6">
+        {searchTerm
+          ? `Found ${filteredTvSeries.length} results for '${searchTerm}'`
+          : "TV Series"}
+      </h1>
 
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-8">
-        {tvSeries.map((series) => (
+        {filteredTvSeries.map((series) => (
           <ShowCard key={series.title} item={series} />
         ))}
       </div>
