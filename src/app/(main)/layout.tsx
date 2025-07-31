95cb3fa3541e8beead605728446d0852ed5b752f
KaanOzen-mF <kaanozn98@gmail.com>
2025-07-31 17:45:24 +0300

    Remove unnecessary comments in layout.tsx for cleaner code

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,3 @@
-// src/app/(main)/layout.tsx (ANA UYGULAMANIN LAYOUT'U)
-
 import Navbar from "@/components/Navbar";
 import { BookmarkProvider } from "@/context/BookmarkContext";
 
@@ -9,9 +7,7 @@ export default function MainAppLayout({
   children: React.ReactNode;
 }) {
   return (
-    // 1. BookmarkProvider, sadece ana uygulama sayfalarını saracak.
     <BookmarkProvider>
-      {/* 2. Tüm layout ve kaydırma sorunlarını çözen en son doğru yapı. */}
       <div className="md:flex md:h-screen bg-darkBlue text-white overflow-auto md:overflow-hidden">
         <header>
           <Navbar />
