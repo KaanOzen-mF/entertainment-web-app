491a99feb7b0209e2eb54e354914315b54aaddc4
KaanOzen-mF <kaanozn98@gmail.com>
2025-08-01 11:26:01 +0300

    Wrap layout in TrailerProvider for improved context management

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import { BookmarkProvider } from "@/context/BookmarkContext";
+import { TrailerProvider } from "@/context/TrailerContext";
 
 export default function MainAppLayout({
   children,
@@ -8,15 +9,17 @@ export default function MainAppLayout({
 }) {
   return (
     <BookmarkProvider>
-      <div className="md:flex md:h-screen bg-darkBlue text-white overflow-auto md:overflow-hidden">
-        <header>
-          <Navbar />
-        </header>
+      <TrailerProvider>
+        <div className="md:flex md:h-screen bg-darkBlue text-white overflow-auto md:overflow-hidden">
+          <header>
+            <Navbar />
+          </header>
 
-        <main className="flex-1 md:overflow-y-auto">
-          <div className="p-4 md:p-6">{children}</div>
-        </main>
-      </div>
+          <main className="flex-1 md:overflow-y-auto">
+            <div className="p-4 md:p-6">{children}</div>
+          </main>
+        </div>
+      </TrailerProvider>
     </BookmarkProvider>
   );
 }
